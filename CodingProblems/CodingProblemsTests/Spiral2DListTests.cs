775bfc709f38e24f9edd4ce96e5efad701c899c9
Raffledoocious <jaydude898@gmail.com>
2014-04-10 23:38:56 -0500

    adding in problem for spiral printing

---
+++ CodingProblems/CodingProblemsTests/Spiral2DListTests.cs
@@ -0,0 +1,24 @@
+﻿using CodingProblems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingProblemsTests
+{
+  [TestClass]
+  public class Spiral2DListTests
+  {
+    [TestMethod]
+    public void Spiral2DList_2by2_CorrectListReturned()
+    {
+      int[,] array = new int[,] { { 1, 2 }, { 3, 4 } };
+      List<int> spiralList = Spiral2DList.Print2DArrayToSpiralList(array);
+      List<int> expectedList = new List<int>() { 1, 2, 4, 3 };
+
+      CollectionAssert.AreEqual(expectedList, spiralList);
+    }
+  }
+}
---
+++ CodingProblems/Spiral2DList.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingProblems
+{
+  public class Spiral2DList
+  {
+    public static List<int> Print2DArrayToSpiralList(int[,] array)
+    {
+      List<int> spiralList = new List<int>();
+      int totalCount = array.GetLength(0) * array.GetLength(1);
+      int printCount = 0;
+      int leftBound = 0;
+      int topBound = 0;
+      int rightBound = array.GetLength(1) - 1;
+      int bottomBound = array.GetLength(0) - 1;
+
+      while (printCount < totalCount)
+      {
+        for (int i = leftBound; i < rightBound; i++)
+        {
+          spiralList.Add(array[0, i]);
+          printCount++;
+        }
+
+        for(int j = topBound; j < bottomBound; j++)
+        {
+          spiralList.Add(array[j, rightBound]);
+          printCount++;
+        }
+
+        for(int k = rightBound; k > leftBound; k--)
+        {
+          spiralList.Add(array[bottomBound, k]);
+          printCount++;
+        }
+
+
+        for(int m = bottomBound; m > topBound; m--)
+        {
+          spiralList.Add(array[m, leftBound]);
+          printCount++;
+        }
+
+        rightBound--;
+        bottomBound--;
+        topBound++;
+        leftBound++;
+      }
+
+      return spiralList;
+    }
+  }
+}
