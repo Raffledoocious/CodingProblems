50724880d49c7d40848877a85d3da810ab3e9cdd
Raffledoocious <jaydude898@gmail.com>
2014-04-11 22:44:07 -0500

    updating tests around string reversal

---
+++ CodingProblems/CodingProblemsTests/ReverseWordsTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodingProblems;
+
+namespace CodingProblemsTests
+{
+  [TestClass]
+  public class ReverseWordsInStringTests
+  {
+    [TestMethod]
+    public void ReverseWords_SingleWord_ShouldReturnSameWord()
+    {
+      char[] sentence = "Alice".ToCharArray();
+      char[] reverse = ReverseWords.ReverseWordsInCharArray(sentence);
+      CollectionAssert.AreEqual(sentence, reverse);
+    }
+
+    [TestMethod]
+    public void ReverseWords_TwoWords_SingleSpace_ReturnsWordsReversed()
+    {
+      char[] sentence = "Alice Likes Bob".ToCharArray();
+      char[] reverse = ReverseWords.ReverseWordsInCharArray(sentence);
+      CollectionAssert.AreEqual("Bob Likes Alice".ToCharArray(), reverse);
+    }
+
+    [TestMethod]
+    public void ReverseWords_MultipleWords_SingleSpace_ReturnsWordsReversed()
+    {
+      char[] sentence = "Alice Likes Bob and woo hoo".ToCharArray();
+      char[] reverse = ReverseWords.ReverseWordsInCharArray(sentence);
+      CollectionAssert.AreEqual("hoo woo and Bob Likes Alice".ToCharArray(), reverse);
+    }
+
+    [TestMethod]
+    public void ReverseWords_EveryOtherASpace_SingleSpace_ReturnsWordsReversed()
+    {
+      char[] sentence = " A b c d e f g ".ToCharArray();
+      char[] reverse = ReverseWords.ReverseWordsInCharArray(sentence);
+      CollectionAssert.AreEqual(" g f e d c b A ".ToCharArray(), reverse);
+    }
+
+    [TestMethod]
+    public void ReverseWords_MultipleWhiteSpaces_ReturnsWordsReversed()
+    {
+      char[] sentence = " A b c d e f g    hi there I am    a  cow   ".ToCharArray();
+      char[] reverse = ReverseWords.ReverseWordsInCharArray(sentence);
+      CollectionAssert.AreEqual("   cow  a    am I there hi    g f e d c b A ".ToCharArray(), reverse);
+    }
+  }
+}
---
+++ CodingProblems/ReverseWords.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingProblems
+{
+   public class ReverseWords
+  {
+     public static char[] ReverseWordsInCharArray(char[] array)
+     {
+       ReverseArrayInRange(ref array, 0, array.Length - 1);
+       int startPtr = 0;
+       int endPtr = 0;
+
+       while (endPtr < array.Length)
+       {
+          while (startPtr < array.Length && !(array[startPtr] != ' '))
+         {
+           startPtr++;
+         }
+
+         endPtr = startPtr;
+
+         while (endPtr < array.Length && array[endPtr] != ' ')
+         {
+           endPtr++;
+         }
+
+         ReverseArrayInRange(ref array, startPtr, endPtr - 1);
+         startPtr = endPtr;
+       }
+
+
+       return array;
+     }
+
+     private static void ReverseArrayInRange(ref char[] array, int start, int end)
+     {
+       int dist = (int) Math.Ceiling( (decimal)(end - start) / 2);
+
+       for (int i = 0; i < dist; i++)
+       {
+         char tmp = array[start + i];
+         array[start + i] = array[end - i];
+         array[end - i] = tmp;
+       }
+     }
+  }
+}
