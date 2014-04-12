955344a4f6cd8214d6426459e20341631fb13414
Raffledoocious <jaydude898@gmail.com>
2014-04-12 15:56:43 -0500

    adding in problem to replace certain characters in limited alphabet solution

---
+++ CodingProblems/CodingProblemsTests/LimitedCharReplaceTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodingProblems;
+
+namespace CodingProblemsTests
+{
+  [TestClass]
+  public class LimitedCharReplaceTests
+  {
+    [TestMethod]
+    public void CharReplace_AllBChar_ArrayShouldBeEmpty()
+    {
+      char[] array = "bbbb".ToCharArray();
+      char[] modified = LimitedCharReplace.ReplaceCharacters(array);
+
+      CollectionAssert.AreEqual("    ".ToCharArray(), modified);
+    }
+    
+    [TestMethod]
+    public void CharReplace_AllAChar_ArrayShouldBeAllDs()
+    {
+      char[] array = "aa  ".ToCharArray();
+      char[] modified = LimitedCharReplace.ReplaceCharacters(array);
+
+      CollectionAssert.AreEqual("dddd".ToCharArray(), modified);
+    }
+  }
+}
---
+++ CodingProblems/LimitedCharReplace.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingProblems
+{
+  /// <summary>
+  /// Problem 6.21
+  /// </summary>
+  public class LimitedCharReplace
+  {
+    public static char[] ReplaceCharacters(char[] array)
+    {
+
+      array = ReplaceBAndShiftArray(array);
+      array = ReplaceAWithDD(array);
+      return array;
+    }
+
+    /// <summary>
+    /// Removes all 'b' characters in the array and shifts characters over 
+    /// resulting in a final array with all 'b' characters removed
+    /// </summary>
+    /// <param name="array">the array to remove characters from</param>
+    /// <returns>array with 'b' char removed</returns>
+    private static char[] ReplaceBAndShiftArray(char[] array)
+    {
+      int startPtr = 0;
+      while (startPtr < array.Length)
+      {
+        while (array[startPtr] == 'b')
+        {
+          int prevPtr = startPtr;
+          for (int i = prevPtr + 1; i < array.Length; i++)
+          {
+            array[prevPtr] = array[i];
+            prevPtr++;
+          }
+          array[prevPtr] = ' ';
+        }
+        startPtr++;
+      }
+      return array;
+    }
+
+    /// <summary>
+    /// Given an array, replaces 'a' character with 'dd',
+    /// other characters are shifted.
+    /// </summary>
+    /// <param name="array">array to replace characters in</param>
+    /// <returns>array with 'a' replaced by 'dd'</returns>
+    private static char[] ReplaceAWithDD(char[] array)
+    {
+      int endPtr = array.Length - 1;
+      int startPtr = 0;
+
+      //replace all characters
+      while (endPtr >= startPtr)
+      {
+        if (array[startPtr] == 'a')
+        {
+          array[startPtr] = ' ';
+          array[endPtr] = 'd';
+          array[endPtr - 1] = 'd';
+          endPtr -= 2;
+          startPtr++;
+        }
+        else
+        {
+          array[endPtr] = array[startPtr];
+          endPtr--;
+          startPtr++;
+        }
+      }
+      if (endPtr >= 0)
+      {
+        //shift array elements to the front based on last end point location
+        startPtr = 0;
+        for (int i = endPtr; i < array.Length - 1; i++)
+        {
+          array[startPtr] = array[endPtr];
+          startPtr++;
+        }
+      }
+
+      return array;
+
+    }
+  }
+}
