c304831ff7a052a3e54ca30a8b75346ce93abd0e
Raffledoocious <jaydude898@gmail.com>
2014-04-27 18:47:44 -0500

    updating problems with max queue problem, though solution is currently incorrect and will have to be revisted

---
+++ CodingProblems/CodingProblemsTests/MaxQueueTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodingProblems;
+
+namespace CodingProblemsTests
+{
+  [TestClass]
+  public class MaxQueueTests
+  {
+    [TestMethod]
+    public void MaxQueue_IncreasingElements_LastElementIsLargest()
+    {
+      MaxQueue<int> queue = new MaxQueue<int>();
+      queue.Enque(1);
+      queue.Enque(2);
+      queue.Enque(3);
+      queue.Enque(4);
+
+      int max = queue.GetMax();
+      Assert.AreEqual(4, max);
+    }
+
+    [TestMethod]
+    public void MaxQueue_AllEqualElementsButOne_MaxElementReturned()
+    {
+      MaxQueue<int> queue = new MaxQueue<int>();
+      queue.Enque(4);
+      queue.Enque(1);
+      queue.Enque(1);
+      queue.Enque(1);
+
+      int max = queue.GetMax();
+      Assert.AreEqual(4, max);      
+    }
+
+    [TestMethod]
+    public void MaxQueue_DequeueMax_MaxUpdatesCorrectly()
+    {
+      MaxQueue<int> queue = new MaxQueue<int>();
+      queue.Enque(6);
+      queue.Enque(4);
+      queue.Enque(4);
+      queue.Enque(5);
+
+      int max = queue.GetMax();
+      Assert.AreEqual(6, max);
+
+      queue.Dequeue();
+      max = queue.GetMax();
+      Assert.AreEqual(5, max);
+
+      queue.Dequeue();
+      max = queue.GetMax();
+      Assert.AreEqual(5, max);
+    }
+  }
+}
---
+++ CodingProblems/MaxQueue.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingProblems
+{
+  /// <summary>
+  /// Currently in progress
+  /// </summary>
+  /// <typeparam name="T">data type of the stack</typeparam>
+  public class MaxQueue<T> where T : IComparable
+  {
+    private Queue<T> queue;
+    
+    // this approach will not work
+    private Stack<T> stack;
+
+    public MaxQueue()
+    {
+      queue = new Queue<T>();
+      stack = new Stack<T>();
+    }
+
+    public void Enque(T element)
+    {
+      queue.Enqueue(element);
+
+      if (stack.Count == 0 || stack.Peek().CompareTo(element) <= 0)
+      {
+        stack.Push(element);
+      }
+    }
+
+    public T Dequeue()
+    {
+      T element = queue.Dequeue();
+      if (stack.Peek().Equals(element))
+      {
+        stack.Pop();
+      }
+
+      return element;
+    }
+
+    public T GetMax()
+    {
+      return stack.Peek();
+    }
+  }
+}
