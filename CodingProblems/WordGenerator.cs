84370d6dde8e0a35011686e6000affe2d955a380
Raffledoocious <jaydude898@gmail.com>
2014-05-05 23:16:54 -0500

    adding in coding problem for a character generator

---
+++ CodingProblems/WordGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingProblems
+{
+  /// <summary>
+  /// From HackerNews interview question
+  /// Given a function which takes a list of characters and an integer,
+  /// generate all combinations of characters
+  /// </summary>
+  public static class CharacterGenerator
+  {
+    public List<String> GenerateCharacterCombinations(List<char> characters, int length)
+    {
+      return null;
+    }
+  }
+}
