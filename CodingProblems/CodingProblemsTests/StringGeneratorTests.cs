2a31a533867db92feccc79f2bcc338e9cba0c1f3
Raffledoocious <jaydude898@gmail.com>
2014-05-07 22:44:11 -0500

    updating tests around string generator

---
+++ CodingProblems/CodingProblemsTests/StringGeneratorTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodingProblems;
+using System.Collections.Generic;
+
+namespace CodingProblemsTests
+{
+  [TestClass]
+  public class StringGeneratorTests
+  {
+    [TestMethod]
+    public void StringGen_TwoCharacters_TwoCharactersReturned()
+    {
+      List<char> chars = new List<char> { 'a', 'b' };
+      List<string> genStrings = StringGenerator.GenerateCharacterCombinations(chars, 1);
+
+      Assert.AreEqual(2, genStrings.Count);
+      Assert.AreEqual("a", genStrings[0]);
+      Assert.AreEqual("b", genStrings[1]);
+    }
+    
+    [TestMethod]
+    public void StringGen_ThreeCharacters_LengthThree_CorrectStringsReturned()
+    {
+      List<char> chars = new List<char> { 'a', 'b', 'c'};
+      List<string> genStrings = StringGenerator.GenerateCharacterCombinations(chars, 3);
+
+      Assert.AreEqual(27, genStrings.Count);
+      CollectionAssert.Contains(genStrings, "aaa");
+      CollectionAssert.Contains(genStrings, "aab");
+      CollectionAssert.Contains(genStrings, "aac");
+      CollectionAssert.Contains(genStrings, "aba");
+      CollectionAssert.Contains(genStrings, "abb");
+      CollectionAssert.Contains(genStrings, "abc");
+      CollectionAssert.Contains(genStrings, "aca");
+      CollectionAssert.Contains(genStrings, "acb");
+      CollectionAssert.Contains(genStrings, "acc");
+      CollectionAssert.Contains(genStrings, "baa");
+      CollectionAssert.Contains(genStrings, "bab");
+      CollectionAssert.Contains(genStrings, "bac");
+      CollectionAssert.Contains(genStrings, "bba");
+      CollectionAssert.Contains(genStrings, "bbb");
+      CollectionAssert.Contains(genStrings, "bbc");
+      CollectionAssert.Contains(genStrings, "bca");
+      CollectionAssert.Contains(genStrings, "bcb");
+      CollectionAssert.Contains(genStrings, "bcc");
+      CollectionAssert.Contains(genStrings, "caa");
+      CollectionAssert.Contains(genStrings, "cab");
+      CollectionAssert.Contains(genStrings, "cac");
+      CollectionAssert.Contains(genStrings, "cba");
+      CollectionAssert.Contains(genStrings, "cbb");
+      CollectionAssert.Contains(genStrings, "cbc");
+      CollectionAssert.Contains(genStrings, "cca");
+      CollectionAssert.Contains(genStrings, "ccb");
+      CollectionAssert.Contains(genStrings, "ccc");
+    }
+  }
+}
