35b403ee2bd2970c8a6306c10a5372e4551aba5d
Raffledoocious <jaydude898@gmail.com>
2014-06-22 01:00:23 -0500

    Updating with get bit count method

---
+++ CodingProblems/GetBitCount.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingProblems
+{
+  public class GetBitCount
+  {
+    /// <summary>
+    /// Given an integer from Int.Min to Int.Max, 
+    /// returns the number of bits set to 1 within it
+    /// </summary>
+    /// <param name="i">int to count bits in</param>
+    /// <returns>number of bits set to 1</returns>
+    public static int GetBitCountFromInt(int num)
+    {
+      int bitCount = 0;
+      //account for 1 in negative signed integers
+      if (num < 0)
+      {
+        bitCount++;
+        //if we are given smallest int
+        //increment and then use the largest int
+        if ( num == int.MinValue)
+        {
+          num = int.MaxValue;
+        }
+        else
+        {
+          num *= -1;
+        }
+      }
+
+      int currentExponent = (int) Math.Floor(Math.Log(num) / Math.Log(2));
+      for (int i = currentExponent; currentExponent >= 0; currentExponent--)
+      {
+        int currentNum = (int) Math.Pow(2, currentExponent);
+        if (num >= currentNum)
+        {
+          bitCount++;
+          num -= currentNum;
+        }
+      }
+
+      return bitCount;
+    }
+  }
+}
