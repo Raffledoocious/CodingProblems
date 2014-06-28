dcd98581528a49e91132a96245a55d467aa6ee10
Raffledoocious <jaydude898@gmail.com>
2014-06-28 17:31:00 -0500

    finishing up bit problem

---
+++ CodingProblems/BitMinMaxFinder.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingProblems
+{
+  public struct MinMaxResult {
+    public int min, max;
+
+    public MinMaxResult(int min, int max)
+    {
+      this.min = min;
+      this.max = max;
+    }
+  }
+
+  public class BitMinMaxFinder
+  {
+    public static MinMaxResult FindMinMaxBasedOnBitCount(int i)
+    {
+      bool[] bits = ConvertToBitArray(i);
+      int maxInt = GetMaxInt(bits);
+      int minInt = GetMinInt(bits);
+      return new MinMaxResult(minInt, maxInt);
+    }
+
+    private static int GetMinInt(bool[] origBits)
+    {
+      bool[] bits = (bool[])origBits.Clone();
+      int lastOne = -1;
+      for (int i = bits.Length - 2; i >= 0; i--)
+      {
+        if (bits[i])
+        {
+          lastOne = i;
+          break;
+        }
+      }
+
+      int lastZero = -1;
+      for (int i = lastOne; i >= 0; i--)
+      {
+        if (!bits[i])
+        {
+          lastZero = i;
+          break;
+        }
+      }
+
+      if (lastZero == -1)
+      {
+        bits = ConvertBitArrayToSmallestNegative(bits, lastOne);
+      }
+      else
+      {
+        bits[lastOne] = !bits[lastOne];
+        bits[lastZero] = !bits[lastZero];
+      }
+
+      return ConvertBitArrayToInt(bits);
+    }
+
+    private static bool[] ConvertBitArrayToSmallestNegative(bool[] bits, int lastOne)
+    {
+      for (int i = 0; i <= lastOne; i++)
+      {
+        bits[lastOne - i] = false;
+        bits[bits.Length - i - 1] = true;
+      }
+
+      return bits;
+    }
+
+    private static int GetMaxInt(bool[] origBits)
+    {
+      bool[] bits = (bool[]) origBits.Clone();
+      int firstOne = -1;
+      for (int i = 0; i < bits.Length; i++)
+      {
+        if (bits[i])
+        {
+          firstOne = i;
+          break;
+        }
+      }
+
+      int firstZero = -1;
+      for (int i = firstOne; i < bits.Length; i++)
+      {
+        if (!bits[i]) 
+        {
+          firstZero = i;
+          break;
+        }
+      }
+
+      int origInt = ConvertBitArrayToInt(origBits);
+      if (origInt % 2 != 0 && origInt != int.MaxValue)
+      {
+        bits[firstZero - 1] = false;
+        bits[firstZero] = true;
+      }
+      else if (firstZero != 31)
+      {
+        bits[firstOne] = !bits[firstOne];
+        bits[firstZero] = !bits[firstZero];
+      }
+
+      return ConvertBitArrayToInt(bits);
+    }
+
+    private static bool[] ConvertToBitArray(int i)
+    {
+      BitArray b = new BitArray(new int[] { i });
+      bool[] bools = new bool[b.Length];
+      b.CopyTo(bools, 0);
+      return bools;
+    }
+
+    private static int ConvertBitArrayToInt(bool[] bits)
+    {
+      BitArray b = new BitArray(bits);
+      int[] intArr = new int[1];
+      b.CopyTo(intArr, 0);
+      return intArr[0];
+    }
+  }
+}
---
+++ CodingProblems/CodingProblemsTests/BitMinMaxFinderTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodingProblems;
+
+namespace CodingProblemsTests
+{
+  [TestClass]
+  public class BitMinMaxFinderTests
+  {
+    [TestMethod]
+    public void MinMax_PassIn2_ShouldReturn_1and4()
+    {
+      MinMaxResult result = BitMinMaxFinder.FindMinMaxBasedOnBitCount(2);
+      Assert.AreEqual(4, result.max);
+      Assert.AreEqual(1, result.min);
+    }
+
+    [TestMethod]
+    public void MinMax_PassIn3_ShouldReturn_5andNeg1073741824()
+    {
+      MinMaxResult result = BitMinMaxFinder.FindMinMaxBasedOnBitCount(3);
+      Assert.AreEqual(5, result.max);
+      Assert.AreEqual(-1073741824, result.min);
+    }
+
+    [TestMethod]
+    public void MinMax_PassInIntMax_ShouldReturn_IntMaxAndNegTwo()
+    {
+      MinMaxResult result = BitMinMaxFinder.FindMinMaxBasedOnBitCount(Int32.MaxValue);
+      Assert.AreEqual(Int32.MaxValue, result.max);
+      Assert.AreEqual(-2, result.min);
+    }
+
+    [TestMethod]
+    public void MinMax_PassIn1073741824_ShouldReturn_SameAnd536870912()
+    {
+      MinMaxResult result = BitMinMaxFinder.FindMinMaxBasedOnBitCount(1073741824);
+      Assert.AreEqual(1073741824, result.max);
+      Assert.AreEqual(536870912, result.min);
+    }
+
+    [TestMethod]
+    public void MinMaxPassIn4_ShouldReturn_8and2()
+    {
+      MinMaxResult result = BitMinMaxFinder.FindMinMaxBasedOnBitCount(4);
+      Assert.AreEqual(8, result.max);
+      Assert.AreEqual(2, result.min);
+    }
+  }
+}
