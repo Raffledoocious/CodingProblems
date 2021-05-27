e8066edbb184299c0f97923c84d13951fb498c64
Jared Lutteke <jlutteke@gmail.com>
2021-05-26 23:10:32 -0700

    Add in initial implementation of ZigZag

---
+++ CodingProblemsTests/ZigZagTests.cs
@@ -0,0 +1,14 @@
+﻿using CodingProblems;
+using NUnit.Framework;
+
+namespace CodingProblemsTests
+{
+	public class ZigZagTests
+	{
+		[Test]
+		public void SeveralRowInput_ReturnsCorrectString()
+		{
+			var result = ZigZag.Convert("PAYPALISHIRING", 3);
+		}
+	}
+}
